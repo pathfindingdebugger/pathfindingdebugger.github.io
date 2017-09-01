3d235de00025bb802c6968e48329d5ea06e30310
surayez <surayez@yahoo.com>
2017-09-01 10:04:41 +1000

    Main webpage creation

--- js/script.js
+++ js/script.js
@@ -8,35 +8,40 @@ var dataReceived;
 
 $(document).ready(function () {
 
+
     $('.buildbtn').click(function () {
         if (dataReceived != null) {
             for (i = currentEventNum; i <= dataReceived.length - 1; i++) {
                 var eventli = document.createElement("LI");
+                eventli.setAttribute("id", i);
                 var newMainItem = document.createTextNode(dataReceived[i].type + ", x= " + dataReceived[i].x + ", y= " + dataReceived[i].y + ", g= " + dataReceived[i].g + ", h= " + dataReceived[i].h);
                 currentEventNum += 1;
-
-                if (dataReceived[i].type == 'expanding') {
-                    var openListli = document.createElement("LI");
-                    openList.push(dataReceived[i].x);
-                    var openListItem = document.createTextNode("[" + openList + "]");
-                    openListli.appendChild(openListItem)
-                }
-
-                if (dataReceived[i].type == 'closing') {
-                    openList.pop(dataReceived[i].x);
-                    var closedListli = document.createElement("LI");
-                    closedList.push(dataReceived[i].x);
-                    var closedListItem = document.createTextNode("[" + closedList + "]");
-                    closedListli.appendChild(closedListItem)
-                }
+                //
+                // if (dataReceived[i].type == 'expanding') {
+                //     var openListli = document.createElement("LI");
+                //     openList.push(dataReceived[i].x);
+                //     var openListItem = document.createTextNode("[" + openList + "]");
+                //     openListli.appendChild(openListItem)
+                // }
+                //
+                // if (dataReceived[i].type == 'closing') {
+                //     openList.pop(dataReceived[i].x);
+                //     var closedListli = document.createElement("LI");
+                //     closedList.push(dataReceived[i].x);
+                //     var closedListItem = document.createTextNode("[" + closedList + "]");
+                //     closedListli.appendChild(closedListItem)
+                // }
 
                 eventli.appendChild(newMainItem);
-                $('#openListConsole').append(openListli);
-                $('#closedListConsole').append(closedListli);
+                // $('#openListConsole').append(openListli);
+                // $('#closedListConsole').append(closedListli);
                 $('#eventList').append(eventli);
             }
             var mydiv = $(".eventLog");
             mydiv.scrollTop(mydiv.prop("scrollHeight"));
+
+
+            $('.graph').load('android.svg');
 
         } else {
             window.alert("No data loaded. Please select file and load data.")
@@ -44,14 +49,18 @@ $(document).ready(function () {
 
     });
 
+    $('#eventList').on('click', 'li', function(ev) {
+        var id = this.id;
+        thisList = document.getElementById(id).innerHTML;
+        console.log("I was clicked! My ID is:"+id);
+        document.getElementById('openList').innerHTML = thisList;
+        document.getElementById('closedList').innerHTML = thisList;
+
+    });
+
+
     $('.playbtn').click(function(){
         console.log("Play button");
-        var eventItems = $('.eventLog');
-        eventItems.each(function(idx, li){
-            var event = $(li);
-            var xcoordinate = event.children(".x").val();
-            console.log(event)
-        })
 
     });
 
