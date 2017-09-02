3a63882eb358a3e363cf4f887515cae5e4c4deb1
Jay <jaypeterwingate@gmail.com>
2017-09-02 13:09:45 +1000

    Test add

--- js/Visualizer.js
+++ js/Visualizer.js
@@ -6,6 +6,15 @@ function drawRectsEvents()
     console.log("THIS IS REGISTERING");
     console.log("Test");
 
+    var rect = new Elem(svg, 'rect')
+        .attr('x', 100).attr('y', 70)
+        .attr('width', 120).attr('height', 80)
+        .attr('fill', '#95B3D7');
+    const animate = setInterval(function() {rect.attr('x', 1 + Number(rect.attr('x')))}, 10);
+    const timer = setInterval(function(){
+        clearInterval(animate);
+        clearInterval(timer);
+        }, 1000);
 }
 
 if (typeof window != 'undefined')
