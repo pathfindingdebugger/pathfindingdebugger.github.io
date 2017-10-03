9d4daba16b2d4c0f2534c1cbef8b08f45a6cbfa1
surayez <surayez@yahoo.com>
2017-10-04 01:22:14 +1100

    Restructuring the site

--- console/js/script.js
+++ console/js/script.js
@@ -20,26 +20,38 @@ const states = {
     Current:6
 };
 
-function changeSpeed(num) {
-    control.stop();
-    playing = false;
+function showList(evt, listName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
-    // if(playing == true){
-    //     console.log("This is working");
-    control.changeSpeed(num);
-    control.play();
-    playing = true;
-    // }
-    // else{
-    //     speed = num;
-    // }
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(listName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
+
+function changeSpeed(num) {
+    control.stop();
+    speed = num;
+    playing = false;
+}
+
+
 $(document).ready(function () {
-    // window.alert("what")
 
     $("#defaultSubmit").click(function(event){
-        console.log(speed);
         $.getJSON('temp2.json', function(currentJSON) {
             eventItems = currentJSON.eventList;
             dataReceived = currentJSON.eventList;
